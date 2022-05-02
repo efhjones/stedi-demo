78e95f5e4d0adbfdeb54cb4d2e7550aefb2b0f8d
Emily Jones <efhjones@gmail.com>
2022-05-02 12:54:36 +0200

    add DefaultMain component

---
+++ components/DefaultMain/DefaultMain.tsx
@@ -0,0 +1,11 @@
+import styles from "./DefaultMain.module.css";
+
+const DefaultMain = () => {
+  return (
+    <section className={styles.root}>
+      <h2>Default Main Component</h2>
+    </section>
+  );
+};
+
+export default DefaultMain;
--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
+export { default as DefaultMain } from "./DefaultMain";
 export { default as Nav } from './Nav'
